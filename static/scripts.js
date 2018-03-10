e58e7e7fdf51d1298e08771d4054c3889dfb734c
ben <ben@workstation.fritz.box>
2018-03-10 14:29:22 +0100

    new tasks-timeline; dark theme; animation;

--- static/scripts.js
+++ static/scripts.js
@@ -1,65 +1,75 @@
+function refresh() {
+    resetTable();
+}
+
 function resetTable() {
-    $('#tableHead th').remove();
-    $('#tableBody tr').remove();
+    $('#containerTable').fadeOut('fast');
+    $('#containerTable').promise().done(function() {
+        $('#tableHead th').remove();
+        $('#tableBody tr').remove();
 
-    $('#tableHead').append('<th></th>');
+        $('#tableHead').append('<th></th>');
 
-    // Load Nodes
-    $.getJSON("/docker/nodes", function (nodes) {
-        nodes.sort(function (a, b) {
-            return a['Description']['Hostname'] > b['Description']['Hostname'];
-        });
-        for (node in nodes) {
-            $('#tableBody').append('\
-            <tr id="' + nodes[node]['ID'] + '"><td style="width: 200px;" scope="row">' + nodes[node]['Description']['Hostname'] + ' (' + nodes[node]['Spec']['Role'] + ')' +
-                '<br/>' + nodes[node]['Status']['Addr'] +
-                '</td></tr>');
-        }
-
-        // Load Services
-        $.getJSON("/docker/services", function (services) {
-            services.sort(function (a, b) {
-                return a['Spec']['Name'] > b['Spec']['Name'];
+        // Load Nodes
+        $.getJSON("/docker/nodes", function (nodes) {
+            nodes.sort(function (a, b) {
+                return a['Description']['Hostname'] > b['Description']['Hostname'] ? 1 : -1;
             });
-            for (service in services) {
-                $('#tableHead').append('<th style="width: 40px;"><div class="rotated">' + services[service]['Spec']['Name'] + '</div></th>');
-                //    id="' + services[service]['ID'] + '"
-                $('#tableBody tr').append('<td id="' + services[service]['ID'] + '"></td>');
+            for (node in nodes) {
+                $('#tableBody').append('\
+            <tr id="' + nodes[node]['ID'] + '"><td class="firstCol" scope="row">' + nodes[node]['Description']['Hostname'] + ' (' + nodes[node]['Spec']['Role'] + ')' +
+                    '<br/>' + nodes[node]['Status']['Addr'] +
+                    '</td></tr>');
             }
 
-            // Load tasks
-            $.getJSON("/docker/tasks", function (tasks) {
-                for (task in tasks) {
-                    taskObject = tasks[task];
-                    if (taskObject['DesiredState'] == 'shutdown') {
-                        continue;
-                    }
-
-                    badgeClass = 'badge-secondary';
-                    if (taskObject['DesiredState'] == 'running') {
-                        badgeClass = 'badge-success';
-                    } else if (taskObject['DesiredState'] == 'failed'
-                        || taskObject['DesiredState'] == 'rejected'
-                        || taskObject['DesiredState'] == 'orphaned') {
-                        badgeClass = 'badge-danger';
-                    } else if (taskObject['DesiredState'] == 'shutdown'
-                        || taskObject['DesiredState'] == 'complete') {
-                        badgeClass = 'badge-dark';
-                    } else if (taskObject['DesiredState'] == 'new'
-                        || taskObject['DesiredState'] == 'ready'
-                        || taskObject['DesiredState'] == 'pending'
-                        || taskObject['DesiredState'] == 'preparing'
-                        || taskObject['DesiredState'] == 'starting') {
-                        badgeClass = 'badge-warning';
-                    }
-
-                    $('#' + taskObject['NodeID'] + ' #' + taskObject['ServiceID']).append('<span class="badge badge-pill ' + badgeClass + '">' + taskObject['DesiredState'] + '</span><br/>');
+            // Load Services
+            $.getJSON("/docker/services", function (services) {
+                services.sort(function (a, b) {
+                    return a['Spec']['Name'] > b['Spec']['Name'] ? 1 : -1;
+                });
+                for (service in services) {
+                    $('#tableHead').append('<th><div class="rotated">' + services[service]['Spec']['Name'] + '</div></th>');
+                    //    id="' + services[service]['ID'] + '"
+                    $('#tableBody tr').append('<td id="' + services[service]['ID'] + '"></td>');
                 }
-                $('thead>tr').append('<th></th>');
-                $('tbody>tr').append('<td></td>');
+
+                // Load tasks
+                $.getJSON("/docker/tasks", function (tasks) {
+                    for (task in tasks) {
+                        taskObject = tasks[task];
+                        state = taskObject['Status']['State']
+                        if (state == 'complete') {
+                            continue;
+                        }
+
+                        badgeClass = 'badge-secondary';
+                        if (state == 'running') {
+                            badgeClass = 'badge-success';
+                        } else if (state == 'failed'
+                            || state == 'rejected'
+                            || state == 'orphaned') {
+                            badgeClass = 'badge-danger';
+                        } else if (state == 'shutdown'
+                            || state == 'complete') {
+                            badgeClass = 'badge-dark';
+                        } else if (state == 'new'
+                            || state == 'ready'
+                            || state == 'pending'
+                            || state == 'preparing'
+                            || state == 'starting') {
+                            badgeClass = 'badge-warning';
+                        }
+
+                        $('#' + taskObject['NodeID'] + ' #' + taskObject['ServiceID']).append('<span class="badge badge-pill ' + badgeClass + '">' + state + '</span><br/>');
+                    }
+                    // $('thead>tr').append('<th></th>');
+                    // $('tbody>tr').append('<td></td>');
+
+                    $('#containerTable').fadeIn('fast');
+                });
             });
-        });
-    });
+        });}
+    );
 }
 
 // while(true) {
@@ -70,10 +80,11 @@ function resetTable() {
 // F5
 // https://stackoverflow.com/questions/4788201/handling-f5-in-jquery
 $(document).bind('keydown keyup', function (e) {
-    // if(e.which === 116) {
+    //if (e.which === 116) {
+    //    console.log('F5 fetched');
     //    resetTable();
     //    return false;
-    // }
+    //}
     // if(e.which === 82 && e.ctrlKey) {
     //    console.log('blocked');
     //    return false;
@@ -82,3 +93,4 @@ $(document).bind('keydown keyup', function (e) {
 
 // First run
 resetTable();
+
---
+++ static/tasksScripts.js
@@ -0,0 +1,58 @@
+function refresh() {
+    resetTasks();
+}
+
+function resetTasks() {
+    $('#tasksTable').fadeOut('fast');
+    $('#tasksTable').promise().done(function () {
+        $('#tasksTable #tableBody tr').remove();
+
+        $.getJSON("/docker/nodes", function (nodes) {
+            $.getJSON("/docker/services", function (services) {
+                $.getJSON("/docker/tasks", function (tasks) {
+                    tasks = tasks.sort(function (a, b) {
+                        return a['Status']['Timestamp'] < b['Status']['Timestamp'] ? 1 : -1;
+                    });
+                    for (task in tasks) {
+                        taskObject = tasks[task];
+                        currentNode = nodes.find(node => node['ID'] == taskObject['NodeID']);
+                        currentService = services.find(service => service['Spec']['TaskTemplate']['ContainerSpec']['Image'] == taskObject['Spec']['ContainerSpec']['Image']);
+
+                        // TODO CopyPaste
+                        state = taskObject['Status']['State']
+                        badgeClass = 'badge-secondary';
+                        if (state == 'running') {
+                            badgeClass = 'badge-success';
+                        } else if (state == 'failed'
+                            || state == 'rejected'
+                            || state == 'orphaned') {
+                            badgeClass = 'badge-danger';
+                        } else if (state == 'shutdown'
+                            || state == 'complete') {
+                            badgeClass = 'badge-dark';
+                        } else if (state == 'new'
+                            || state == 'ready'
+                            || state == 'pending'
+                            || state == 'preparing'
+                            || state == 'starting') {
+                            badgeClass = 'badge-warning';
+                        }
+
+                        $('#tableBody').append('\
+            <tr>\
+            <td>'+ new Date(taskObject['Status']['Timestamp']).toLocaleString() + '</td>\
+            <td><span class="badge badge-pill ' + badgeClass + '">' + state + '</span></td>\
+            <td>'+ taskObject['DesiredState'] + '</td>\
+            <td>'+ currentNode['Description']['Hostname'] + '</td>\
+            <td>'+ currentService['Spec']['Name'] +'</td>\
+            </tr>');
+                    }
+
+                    $('#tasksTable').fadeIn('fast');
+                });
+            });
+        });
+    });
+}
+
+resetTasks();
