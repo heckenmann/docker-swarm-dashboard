dc42086946e89e1242264984552ec1f2b1754142
ben <ben@workstation.fritz.box>
2018-03-31 20:57:54 +0200

    refactoring to react

---
+++ app-src/src/components/DashboardNavbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, NavItem, MenuItem, NavDropdown, Button, FormGroup, FormControl } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import FontAwesome from 'react-fontawesome';
+import logo from '../docker.png';
+import { ServicesComponent } from './ServicesComponent';
+
+class DashboardNavbar extends Component {
+    render() {
+        return (
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand bsclass="white-space: nowrap;">
+                        <img alt="logo" id="dockerlogo" src={logo} /> Docker Swarm Dashboard
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Nav>
+                    <LinkContainer to="/services">
+                        <NavItem><FontAwesome name="linode" /> Services / Nodes</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/tasks">
+                        <NavItem><FontAwesome name="tasks" /> Tasks</NavItem>
+                    </LinkContainer>
+                </Nav>
+                <Nav pullRight>
+                    <LinkContainer to="/about">
+                        <NavItem><FontAwesome name="info-circle" /> About</NavItem>
+                    </LinkContainer>
+                </Nav>
+            </Navbar>
+        );
+    }
+}
+
+export { DashboardNavbar };
---
+++ app-src/src/components/TasksComponent.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Table, Label } from 'react-bootstrap';
+import { getStyleClassForState } from '../Helper';
+
+class TasksComponent extends Component {
+
+    state = {
+        initialized: false,
+        nodes: [],
+        services: [],
+        tasks: []
+    }
+
+    async loadData() {
+        let localState = this.state;
+        localState.nodes = (await (await fetch("/docker/nodes")).json()).sort((a, b) => { return a['Description']['Hostname'] > b['Description']['Hostname'] ? 1 : -1; });
+        localState.services = (await (await fetch("/docker/services")).json()).sort((a, b) => { return a['Spec']['Name'] > b['Spec']['Name'] ? 1 : -1; });
+        localState.tasks = (await (await fetch("/docker/tasks")).json()).sort((a, b) => { return a['Status']['Timestamp'] < b['Status']['Timestamp'] ? 1 : -1; });
+        localState.initialized = true;
+        this.setState(localState);
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    render() {
+        let rows = this.state.tasks.map(task => {
+            let currentNode = this.state.nodes.find(node => node['ID'] === task['NodeID']);
+            let currentService = this.state.services.find(service => service['ID'] == task['ServiceID']);
+
+            let currentNodeName = currentNode == null ? "" : currentNode['Description']['Hostname'];
+            let currentServiceName = currentService == null ? "" : currentService['Spec']['Name'];
+            let currentError = task['Status']['Err'] == null ? "" : task['Status']['Err'];
+            return (
+                <tr>
+                    <td>{new Date(task['Status']['Timestamp']).toLocaleString()}</td>
+                    <td><Label bsStyle={getStyleClassForState(task['Status']['State'])} className="ds-label">{task['Status']['State']} </Label></td>
+                    <td>{task['DesiredState']}</td>
+                    <td>{currentServiceName}</td>
+                    <td>{currentNodeName}</td>
+                    <td>{currentError}</td>
+                </tr>
+            );
+        });
+        return (
+            <Table striped bordered condensed hover id="tasksTable">
+                <thead>
+                    <tr>
+                        <th id="timestampCol">Timestamp</th>
+                        <th id="stateCol">State</th>
+                        <th id="desiredstateCol">DesiredState</th>
+                        <th id="serviceCol">ServiceName</th>
+                        <th id="nodeCol">Node</th>
+                        <th>Error</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </Table>
+        )
+    }
+
+}
+
+export { TasksComponent };
