8cd879e634a40674f6d6be5e91dd84fa5f68ff20
ben <ben@workstation.fritz.box>
2018-04-01 18:18:06 +0200

    global state

--- app-src/src/App.js
+++ app-src/src/App.js
@@ -14,16 +14,42 @@ import bg from './docker.png';
 import { Button } from 'react-bootstrap';
 
 class App extends Component {
+
+  state = {
+    time: new Date(),
+    initialized: false,
+    nodes: [],
+    services: [],
+    tasks: []
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  update = () => {
+    this.loadData();
+  }
+
+  async loadData() {
+    let localState = this.state;
+    localState.nodes = (await (await fetch("/docker/nodes")).json()).sort((a, b) => { return a['Description']['Hostname'] > b['Description']['Hostname'] ? 1 : -1; });
+    localState.services = (await (await fetch("/docker/services")).json()).sort((a, b) => { return a['Spec']['Name'] > b['Spec']['Name'] ? 1 : -1; });
+    localState.tasks = (await (await fetch("/docker/tasks")).json()).sort((a, b) => { return a['Status']['Timestamp'] < b['Status']['Timestamp'] ? 1 : -1; });
+    localState.initialized = true;
+    this.setState(localState);
+  }
+
   render() {
     return (
       <HashRouter>
         <div className="App">
           <img alt="background" id="background-image" src={bg} />
-          <DashboardNavbar />
+          <DashboardNavbar forceUpdate={this.update} />
           <Switch>
-            <Route exact path='/' component={ServicesComponent} />
-            <Route exact path='/services' component={ServicesComponent} />
-            <Route exact path='/tasks' component={TasksComponent} />
+            <Route exact path='/' component={() => (<ServicesComponent state={this.state} />)} />
+            <Route exact path='/services' component={() => (<ServicesComponent state={this.state} />)} />
+            <Route exact path='/tasks' component={() => (<TasksComponent state={this.state} />)} />
             <Route exact path='/about' component={AboutComponent} />
           </Switch>
         </div>
--- app-src/src/components/TasksComponent.js
+++ app-src/src/components/TasksComponent.js
@@ -4,30 +4,10 @@ import { getStyleClassForState } from '../Helper';
 
 class TasksComponent extends Component {
 
-    state = {
-        initialized: false,
-        nodes: [],
-        services: [],
-        tasks: []
-    }
-
-    async loadData() {
-        let localState = this.state;
-        localState.nodes = (await (await fetch("/docker/nodes")).json()).sort((a, b) => { return a['Description']['Hostname'] > b['Description']['Hostname'] ? 1 : -1; });
-        localState.services = (await (await fetch("/docker/services")).json()).sort((a, b) => { return a['Spec']['Name'] > b['Spec']['Name'] ? 1 : -1; });
-        localState.tasks = (await (await fetch("/docker/tasks")).json()).sort((a, b) => { return a['Status']['Timestamp'] < b['Status']['Timestamp'] ? 1 : -1; });
-        localState.initialized = true;
-        this.setState(localState);
-    }
-
-    componentDidMount() {
-        this.loadData();
-    }
-
     render() {
-        let rows = this.state.tasks.map(task => {
-            let currentNode = this.state.nodes.find(node => node['ID'] === task['NodeID']);
-            let currentService = this.state.services.find(service => service['ID'] === task['ServiceID']);
+        let rows = this.props.state.tasks.map(task => {
+            let currentNode = this.props.state.nodes.find(node => node['ID'] === task['NodeID']);
+            let currentService = this.props.state.services.find(service => service['ID'] === task['ServiceID']);
 
             let currentNodeName = currentNode == null ? "" : currentNode['Description']['Hostname'];
             let currentServiceName = currentService == null ? "" : currentService['Spec']['Name'];
