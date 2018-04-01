5eaa7c4a0002a262fa27beb6e2a6c2114f23f645
ben <ben@workstation.fritz.box>
2018-04-02 00:47:13 +0200

    Dockerfile

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
+#!/bin/sh
+
 apk update
 apk add --no-cache git nodejs-npm
 npm install --production
 npm build
-go get "github.com/docker/docker/api/types" "github.com/docker/docker/client" "github.com/gorilla/mux" "golang.org/x/net/context"
+go get "github.com/docker/docker/api/types"
+go get "github.com/docker/docker/client"
+go get "github.com/gorilla/mux"
+go get "golang.org/x/net/context"
 go build dockerswarmdashboard.go
 apk del git nodejs-npm go
 rm -r /go /usr/local/go
