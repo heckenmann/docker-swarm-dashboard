012a0d71c18a84270fb8043fb9c54c30fa8d6d46
ben <ben@workstation.fritz.box>
2018-04-02 01:08:34 +0200

    Dockerfile

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ apk update
 apk add --no-cache git nodejs-npm wget go
 wget http://getcarina.github.io/jupyterhub-tutorial/slides/img/docker-swarm.png -O /opt/dsd/src/docker.png
 npm install --only=production
-npm build
+npm run build
 go get "github.com/docker/docker/api/types"
 go get "github.com/docker/docker/client"
 go get "github.com/gorilla/mux"
