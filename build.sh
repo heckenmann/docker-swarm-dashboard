7e06e75d86a59a5db2608f20024d55f71ca20e17
ben <ben@workstation.fritz.box>
2018-04-02 10:16:29 +0200

    Dockerfile Error

--- build.sh
+++ build.sh
@@ -4,12 +4,15 @@ cd /opt/dsd
 ls -la
 #apk update && apk upgrade
 apk add --no-cache git nodejs-npm wget go
-wget http://getcarina.github.io/jupyterhub-tutorial/slides/img/docker-swarm.png -O /opt/dsd/src/docker.png
+wget --quiet http://getcarina.github.io/jupyterhub-tutorial/slides/img/docker-swarm.png -O /opt/dsd/src/docker.png
 npm install --only=production
-npm run build
+npm run-script build
+rm -r /opt/dsd/node_modules
+
 go get "github.com/docker/docker/api/types"
 go get "github.com/docker/docker/client"
 go get "github.com/gorilla/mux"
 go get "golang.org/x/net/context"
 go build dockerswarmdashboard.go
+
 apk del git nodejs-npm go wget
