fcf9b8fee3983108e1221ffdbd56c1dbcd4515e9
ben <ben@workstation.fritz.box>
2018-04-14 15:14:48 +0200

    Navbar staticTop

--- app-src/src/components/DashboardNavbar.js
+++ app-src/src/components/DashboardNavbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavItem, MenuItem, NavDropdown, Button, FormGroup, FormControl } from 'react-bootstrap';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import faSync from '@fortawesome/fontawesome-free-solid/faSync';
@@ -7,13 +7,12 @@ import faLinode from '@fortawesome/fontawesome-free-brands/faLinode';
 import faTasks from '@fortawesome/fontawesome-free-solid/faTasks';
 import faInfoCircle from '@fortawesome/fontawesome-free-solid/faInfoCircle';
 import logo from '../docker.png';
-import { ServicesComponent } from './ServicesComponent';
 
 class DashboardNavbar extends Component {
 
     render() {
         return (
-            <Navbar inverse fluid>
+            <Navbar inverse staticTop>
                 <Navbar.Header>
                     <Navbar.Brand bsclass="white-space: nowrap;">
                         <img alt="logo" id="dockerlogo" src={logo} /> Docker Swarm Dashboard
