06b8988996f8cb80f784a95a9cacb99a4c26e382
heckenmann <benedikt.stoffel@heckenmann.de>
2022-03-12 17:50:23 +0100

    change react-components to functions/hooks

--- app-src/src/components/AboutComponent.js
+++ app-src/src/components/AboutComponent.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
-import { Table, Card, Jumbotron } from 'react-bootstrap';
+import { Table, Card } from 'react-bootstrap';
 
-class AboutComponent extends Component {
-
-    render() {
-        return (
-            <Card bg='light'>
-                <Card.Body>
-                    <h1>
-                        Docker Swarm Dashboard <small>by heckenmann</small>
-                    </h1>
-                    <Table>
-                        <tbody>
-                            <tr>
-                                <td>License:</td>
-                                <td><a href="https://github.com/heckenmann/docker-swarm-dashboard/blob/master/LICENSE">https://github.com/heckenmann/docker-swarm-dashboard/blob/master/LICENSE</a></td>
-                            </tr>
-                            <tr>
-                                <td>GitHub-Project:</td>
-                                <td><a href="https://github.com/heckenmann/docker-swarm-dashboard">https://github.com/heckenmann/docker-swarm-dashboard</a></td>
-                            </tr>
-                            <tr>
-                                <td>Docker Registry:</td>
-                                <td><a href="https://github.com/heckenmann/docker-swarm-dashboard/pkgs/container/docker-swarm-dashboard">https://github.com/heckenmann/docker-swarm-dashboard/pkgs/container/docker-swarm-dashboard</a></td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </Card.Body>
-            </Card >
-        );
-    }
+function AboutComponent() {
+    return (
+        <Card bg='light'>
+            <Card.Body>
+                <h1>
+                    Docker Swarm Dashboard <small>by heckenmann</small>
+                </h1>
+                <Table>
+                    <tbody>
+                        <tr>
+                            <td>License:</td>
+                            <td><a href="https://github.com/heckenmann/docker-swarm-dashboard/blob/master/LICENSE">https://github.com/heckenmann/docker-swarm-dashboard/blob/master/LICENSE</a></td>
+                        </tr>
+                        <tr>
+                            <td>GitHub-Project:</td>
+                            <td><a href="https://github.com/heckenmann/docker-swarm-dashboard">https://github.com/heckenmann/docker-swarm-dashboard</a></td>
+                        </tr>
+                        <tr>
+                            <td>Docker Registry:</td>
+                            <td><a href="https://github.com/heckenmann/docker-swarm-dashboard/pkgs/container/docker-swarm-dashboard">https://github.com/heckenmann/docker-swarm-dashboard/pkgs/container/docker-swarm-dashboard</a></td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </Card.Body>
+        </Card >
+    );
 }
 
 export { AboutComponent };
--- app-src/src/components/TasksComponent.js
+++ app-src/src/components/TasksComponent.js
@@ -1,53 +1,48 @@
-import React, { Component } from 'react';
 import { Table, Badge, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { toDefaultDateTimeString } from '../common/DefaultDateTimeFormat';
 import { getStyleClassForState } from '../Helper';
 
-class TasksComponent extends Component {
+function TasksComponent(props) {
+    let rows = props.state.tasks.map(task => {
+        let currentNode = props.state.nodes.find(node => node['ID'] === task['NodeID']);
+        let currentService = props.state.services.find(service => service['ID'] === task['ServiceID']);
 
-    render() {
-        let rows = this.props.state.tasks.map(task => {
-            let currentNode = this.props.state.nodes.find(node => node['ID'] === task['NodeID']);
-            let currentService = this.props.state.services.find(service => service['ID'] === task['ServiceID']);
-
-            let currentNodeName = currentNode == null ? "" : currentNode['Description']['Hostname'];
-            let currentServiceName = currentService == null ? "" : currentService['Spec']['Name'];
-            let currentError = task['Status']['Err'] == null ? "" : task['Status']['Err'];
-            return (
-                <tr key={'tasksTable-' + task['ID']}>
-                    <td>{toDefaultDateTimeString(new Date(task['Status']['Timestamp']))}</td>
-                    <td><Badge className='w-100' bg={getStyleClassForState(task['Status']['State'])}>{task['Status']['State']} </Badge></td>
-                    <td>{task['DesiredState']}</td>
-                    <td><Link to={'/services/' + currentService.ID}>{currentServiceName}</Link></td>
-                    <td><Link to={'/nodes/' + currentNode.ID}>{currentNodeName}</Link></td>
-                    <td>{currentError}</td>
-                </tr>
-            );
-        });
+        let currentNodeName = currentNode == null ? "" : currentNode['Description']['Hostname'];
+        let currentServiceName = currentService == null ? "" : currentService['Spec']['Name'];
+        let currentError = task['Status']['Err'] == null ? "" : task['Status']['Err'];
         return (
-            <Card bg='light'>
-                <Card.Body>
-                    <Table striped hover id="tasksTable" size="sm">
-                        <thead>
-                            <tr>
-                                <th id="timestampCol">Timestamp</th>
-                                <th id="stateCol">State</th>
-                                <th id="desiredstateCol">DesiredState</th>
-                                <th id="serviceCol">ServiceName</th>
-                                <th id="nodeCol">Node</th>
-                                <th>Error</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {rows}
-                        </tbody>
-                    </Table>
-                </Card.Body>
-            </Card>
-        )
-    }
-
+            <tr key={'tasksTable-' + task['ID']}>
+                <td>{toDefaultDateTimeString(new Date(task['Status']['Timestamp']))}</td>
+                <td><Badge className='w-100' bg={getStyleClassForState(task['Status']['State'])}>{task['Status']['State']} </Badge></td>
+                <td>{task['DesiredState']}</td>
+                <td><Link to={'/services/' + currentService.ID}>{currentServiceName}</Link></td>
+                <td><Link to={'/nodes/' + currentNode.ID}>{currentNodeName}</Link></td>
+                <td>{currentError}</td>
+            </tr>
+        );
+    });
+    return (
+        <Card bg='light'>
+            <Card.Body>
+                <Table striped hover id="tasksTable" size="sm">
+                    <thead>
+                        <tr>
+                            <th id="timestampCol">Timestamp</th>
+                            <th id="stateCol">State</th>
+                            <th id="desiredstateCol">DesiredState</th>
+                            <th id="serviceCol">ServiceName</th>
+                            <th id="nodeCol">Node</th>
+                            <th>Error</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </Table>
+            </Card.Body>
+        </Card>
+    )
 }
 
 export { TasksComponent };
