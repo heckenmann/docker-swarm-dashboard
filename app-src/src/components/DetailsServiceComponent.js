cf8c757d60c270eabfc19e901d6fc010b1c571f5
heckenmann <benedikt.stoffel@heckenmann.de>
2022-03-12 16:47:04 +0100

    first version of detail-pages

---
+++ app-src/src/components/DetailsServiceComponent.js
@@ -0,0 +1,32 @@
+import { Card, Tab, Tabs } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useParams } from 'react-router-dom';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import docco from 'react-syntax-highlighter/dist/esm/styles/hljs/docco';
+import { JsonToTable } from 'react-json-to-table';
+import cleanDeep from 'clean-deep';
+
+function DetailsServiceComponent(props) {
+    if (!props?.state?.initialized) return <div></div>;
+    let { id } = useParams();
+    let currentService = props.state.services.find(s => s.ID == id);
+    if (!currentService) return <div>Service doesn't exist</div>;
+
+    return (
+        <Card bg='light'>
+            <Card.Header><h5><FontAwesomeIcon icon="folder" /> Service "{currentService.Spec?.Name}"</h5></Card.Header>
+            <Card.Body>
+                <Tabs className="mb-3">
+                    <Tab eventKey="table" title="Table">
+                        <JsonToTable  json={cleanDeep(currentService)} />
+                    </Tab>
+                    <Tab eventKey="json" title="JSON">
+                        <SyntaxHighlighter language="javascript" style={docco}>{JSON.stringify(currentService, null, "\t")}</SyntaxHighlighter>
+                    </Tab>
+                </Tabs>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export { DetailsServiceComponent };
