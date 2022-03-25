58fb86bdf801b56c8427a72ac2c14766a259f82b
heckenmann <benedikt.stoffel@heckenmann.de>
2022-03-25 23:52:49 +0100

    use hook; add NodeComponent

---
+++ app-src/src/components/NodesComponent.js
@@ -0,0 +1,67 @@
+import { Table, Badge, Button, Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+
+function NodesComponent(props) {
+    if (!props || !props.isInitialized) {
+        return (<div></div>);
+    }
+    let theads = [];
+    let trows = [];
+
+    theads.push(<th key='serviceTable-empty'></th>);
+
+    // Rows
+    props.nodes.forEach(node => {
+        trows.push(
+            <tr key={'tr' + node['ID']} className={node['Status']['State'] === 'ready' ? null : 'danger'}>
+                <td className='align-middle text-nowrap'>
+                    <Link to={'/nodes/' + node['ID']} >{node['Description']['Hostname']}  {node['ManagerStatus'] && node['ManagerStatus']['Leader'] && <FontAwesomeIcon icon='star' />}</Link>
+                </td>
+                <td className='align-middle col-md-1'>{node['Spec']['Role']}</td>
+                <td className='align-middle col-md-1'>
+                    {
+                        node['Status']['State'] === 'ready' &&
+                        <Badge bg="success" className='w-100'>Ready</Badge>
+                        ||
+                        node['Status']['State'] === 'down' &&
+                        <Badge bg='danger' className='w-100'>Down</Badge>
+                        ||
+                        <Badge bg='warning' className='w-100'>{node['Status']['State']}</Badge>
+                    }
+                </td>
+                <td className='align-middle col-md-1'>
+                    {
+                        node['Spec']['Availability'] === 'active' && <Badge bg='success' className='w-100'>{node['Spec']['Availability']}</Badge>
+                        ||
+                        <Badge bg='warning' className='w-100'>{node['Spec']['Availability']}</Badge>
+                    }
+                </td>
+                <td className='align-middle col-md-1'>{node['ManagerStatus'] ? node['ManagerStatus']['Addr']?.split(':')[0] : node['Status']['Addr']}</td>
+            </tr>
+        );
+    });
+
+    return (
+        <Card bg='light'>
+            <Card.Body>
+                <Table key="nodesTable" id="nodesTable" size='sm' striped hover>
+                    <thead>
+                        <tr>
+                            <th className="nodeAttribute">Node</th>
+                            <th className="nodeAttributeSmall">Role</th>
+                            <th className="nodeAttributeSmall">State</th>
+                            <th className="nodeAttributeSmall">Availability</th>
+                            <th className="nodeAttributeSmall">IP</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {trows}
+                    </tbody>
+                </Table>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export { NodesComponent };
