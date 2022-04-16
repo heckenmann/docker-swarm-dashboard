c586299e1621429d19aadb805d61f1b3575c9d44
heckenmann <benedikt.stoffel@heckenmann.de>
2022-04-16 14:19:51 +0200

    fix logs-component

--- app-src/src/common/store/reducers.js
+++ app-src/src/common/store/reducers.js
@@ -1,14 +1,14 @@
 export const RefreshIntervalToggleReducer = (prev) => {
-    return prev ? null : 1000;
+    return prev ? null : 3000;
 }
 
 export const MessageReducer = (prev, action) => {
-    if(action.type === 'add') {
+    if (action.type === 'add') {
         const next = prev.filter(e => e !== action.value);
         next.push(action.value);
         return next;
     };
-    if(action.type === 'remove') {
+    if (action.type === 'remove') {
         const next = prev.filter(e => e !== action.value);
         return next;
     };
