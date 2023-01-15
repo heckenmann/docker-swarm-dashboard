95cbf2f53da455efedae0361308337b02ee2dc1a
heckenmann <benedikt.stoffel@heckenmann.de>
2023-01-15 17:20:46 +0100

    Add "tools" and "libs" to README.md and AboutComponent.js

--- app-src/src/components/AboutComponent.js
+++ app-src/src/components/AboutComponent.js
@@ -1,6 +1,9 @@
 import { useAtomValue } from 'jotai';
-import { Table, Card } from 'react-bootstrap';
+import {Table, Card, Button} from 'react-bootstrap';
 import { currentVariantAtom, currentVariantClassesAtom } from '../common/store/atoms';
+import jb_square from '../jb_square.svg';
+import goLand_icon from '../GoLand_icon.svg';
+import webStorm_icon from '../WebStorm_icon.svg';
 
 function AboutComponent() {
     const currentVariant = useAtomValue(currentVariantAtom);
@@ -29,6 +32,19 @@ function AboutComponent() {
                         </tr>
                     </tbody>
                 </Table>
+                <h2>Tools</h2>
+                <p>Special thanks to JetBrains for supporting this project with <a href="https://www.jetbrains.com/community/opensource/#support" target="_blank">Open Source development licenses</a>.</p>
+                <a href="https://www.jetbrains.com/" target="_blank"><img src={jb_square}  height="80px" alt="JetBrains Black Box Logo logo." /></a>
+                <a href="https://www.jetbrains.com/go/" target="_blank"><img src={goLand_icon} height="80px" alt="GoLand logo." /></a>
+                <a href="https://www.jetbrains.com/webstorm/" target="_blank"><img src={webStorm_icon} height="80px" alt="WebStorm logo." /></a>
+                <h2>Frameworks & Libraries</h2>
+                <ul>
+                    <li><Button variant="link" target="_blank" href="https://getbootstrap.com/">Bootstrap</Button></li>
+                    <li><Button variant="link" target="_blank" href="https://fontawesome.com/">Fontawesome</Button></li>
+                    <li><Button variant="link" target="_blank" href="https://www.gorillatoolkit.org/">Gorilla</Button></li>
+                    <li><Button variant="link" target="_blank" href="https://jotai.org/">Jotai</Button></li>
+                    <li><Button variant="link" target="_blank" href="https://reactjs.org/">React</Button></li>
+                </ul>
             </Card.Body>
         </Card >
     );
