dde0b3978a4c3ddd147bf98698b20cfcefe451e5
heckenmann <benedikt.stoffel@heckenmann.de>
2023-10-21 10:47:06 +0200

    base url as setting and atomWithHash

--- app-src/src/common/store/atoms.js
+++ app-src/src/common/store/atoms.js
@@ -2,22 +2,33 @@ import { atom } from 'jotai'
 import { atomWithReducer, atomWithReset, selectAtom } from 'jotai/utils'
 import a11yDark from 'react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark'
 import a11yLight from 'react-syntax-highlighter/dist/esm/styles/hljs/a11y-light'
-import { baseUrl, fetchNodes, fetchServices, fetchTasks } from '../network'
 import { MessageReducer, RefreshIntervalToggleReducer } from './reducers'
 import { atomWithHash } from 'jotai-location'
-import { dashboardHId } from '../navigationConstants'
+import { dispatcherId } from '../navigationConstants'
+
+// Initiale Werte
+const hash = window.location.hash
+const hashWithoutHash = hash.substring(1)
+
+// Den Hash-Teil in eine Objektstruktur parsen
+const parsedHash = hashWithoutHash
+  .split('&')
+  .map((pair) => pair.split('='))
+  .reduce((acc, [key, value]) => {
+    acc[key] = decodeURIComponent(value)
+    return acc
+  }, {})
 
 // Jotai-Atoms
+export const baseUrlAtom = atomWithHash(
+  'base',
+  parsedHash.base ? parsedHash.base.replaceAll('"', '') : '/',
+)
 export const refreshIntervalAtom = atomWithReducer(
   null,
   RefreshIntervalToggleReducer,
 )
-export const nodesAtom = atomWithReducer(fetchNodes(), () => fetchNodes())
-export const servicesAtom = atomWithReducer(fetchServices(), () =>
-  fetchServices(),
-)
-export const tasksAtom = atomWithReducer(fetchTasks(), () => fetchTasks())
-export const viewAtom = atomWithHash('view', { id: dashboardHId })
+export const viewAtom = atomWithHash('view', { id: dispatcherId })
 export const messagesAtom = atomWithReducer([], MessageReducer)
 export const tableSizeAtom = atomWithHash('tablesize', 'sm')
 
@@ -25,51 +36,49 @@ export const tableSizeAtom = atomWithHash('tablesize', 'sm')
 export const dashboardHAtom = atom(async (get) => {
   // Reload when view changed
   get(viewAtom)
-  return await fetch(baseUrl + 'ui/dashboardh').then((data) => data.json())
+  return (await fetch(get(baseUrlAtom) + 'ui/dashboardh')).json()
 })
 export const dashboardVAtom = atom(async (get) => {
   // Reload when view changed
   get(viewAtom)
-  return await fetch(baseUrl + 'ui/dashboardv').then((data) => data.json())
+  return (await fetch(get(baseUrlAtom) + 'ui/dashboardv')).json()
 })
 export const stacksAtom = atom(async (get) => {
   // Reload when view changed
   get(viewAtom)
-  return await fetch(baseUrl + 'ui/stacks').then((data) => data.json())
+  return (await fetch(get(baseUrlAtom) + 'ui/stacks')).json()
 })
 export const portsAtom = atom(async (get) => {
   // Reload when view changed
   get(viewAtom)
-  return await fetch(baseUrl + 'ui/ports').then((data) => data.json())
+  return (await fetch(get(baseUrlAtom) + 'ui/ports')).json()
 })
 export const nodesAtomNew = atom(async (get) => {
   // Reload when view changed
   get(viewAtom)
-  return await fetch(baseUrl + 'ui/nodes').then((data) => data.json())
+  return (await fetch(get(baseUrlAtom) + 'ui/nodes')).json()
 })
 export const tasksAtomNew = atom(async (get) => {
   // Reload when view changed
   get(viewAtom)
-  return await fetch(baseUrl + 'ui/tasks').then((data) => data.json())
+  return (await fetch(get(baseUrlAtom) + 'ui/tasks')).json()
 })
 export const nodeDetailAtom = atom(async (get) => {
   let id = get(viewAtom)['detail']
-  return await fetch(baseUrl + 'docker/nodes/' + id).then((data) => data.json())
+  return (await fetch(get(baseUrlAtom) + 'docker/nodes/' + id)).json()
 })
 export const logsServicesAtom = atom(async (get) => {
   // Reload when view changed
   get(viewAtom)
-  return await fetch(baseUrl + 'ui/logs/services').then((data) => data.json())
+  return (await fetch(get(baseUrlAtom) + 'ui/logs/services')).json()
 })
 export const serviceDetailAtom = atom(async (get) => {
   let id = get(viewAtom)['detail']
-  return await fetch(baseUrl + 'docker/services/' + id).then((data) =>
-    data.json(),
-  )
+  return (await fetch(get(baseUrlAtom) + 'docker/services/' + id)).json()
 })
 export const taskDetailAtom = atom(async (get) => {
   let id = get(viewAtom)['detail']
-  return await fetch(baseUrl + 'docker/tasks/' + id).then((data) => data.json())
+  return (await fetch(get(baseUrlAtom) + 'docker/tasks/' + id)).json()
 })
 
 // Logs
