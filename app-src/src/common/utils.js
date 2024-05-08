92c76412b676d6e3cbcfa35239fff34db26192b8
Vegard Hagen <vegard.hagen@spk.no>
2024-05-08 14:57:44 +0200

    feat: Filter services by service or stack name

--- app-src/src/common/utils.js
+++ app-src/src/common/utils.js
@@ -9,3 +9,26 @@ export const flatten = (obj, path = '') => {
       : { ...output, ...flatten(obj[key], path + key + '.') }
   }, {})
 }
+
+export const serviceFilter = (service, serviceNameFilter, stackNameFilter) => {
+  if (!serviceNameFilter && !stackNameFilter) {
+    return true
+  }
+  const { Name: serviceName, Stack: stackName } = service
+  const filterName = serviceNameFilter
+    ? serviceName.includes(serviceNameFilter)
+    : true
+  const filterStack = stackNameFilter
+    ? stackName.includes(stackNameFilter)
+    : true
+  if (serviceNameFilter && !stackNameFilter) {
+    return filterName
+  }
+  if (stackNameFilter && !serviceNameFilter) {
+    return filterStack
+  }
+  if (serviceNameFilter && stackNameFilter) {
+    return filterName && filterStack
+  }
+  return true
+}
