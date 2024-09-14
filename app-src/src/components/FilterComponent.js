321f255924cc21625a1c6cdc3aec2f881ed65b98
heckenmann <benedikt.stoffel@heckenmann.de>
2024-09-14 19:45:04 +0200

    add form to FilterComponent.js

--- app-src/src/components/FilterComponent.js
+++ app-src/src/components/FilterComponent.js
@@ -1,13 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button, Form, InputGroup } from 'react-bootstrap'
 import { useState } from 'react'
-import { useAtom } from 'jotai/index'
+import { useAtom, useAtomValue } from 'jotai/index'
 import {
+  currentVariantAtom,
   serviceNameFilterAtom,
   stackNameFilterAtom,
 } from '../common/store/atoms'
 
 function FilterComponent() {
+  const variant = useAtomValue(currentVariantAtom)
   const [serviceFilter, setServiceNameFilter] = useAtom(serviceNameFilterAtom)
   const [stackFilter, setStackNameFilter] = useAtom(stackNameFilterAtom)
   const [filterType, setFilterType] = useState(
@@ -34,34 +36,38 @@ function FilterComponent() {
   }
 
   return (
-    <InputGroup className="d-flex flex-nowrap">
-      <InputGroup.Text>
-        <FontAwesomeIcon icon="filter" />
-      </InputGroup.Text>
-      <Form.Select
-        className="w-auto"
-        value={filterType}
-        onChange={(event) => {
-          changeFilterType(event.target.value)
-        }}
-      >
-        <option value="service">Service</option>
-        <option value="stack">Stack</option>
-      </Form.Select>
-      <Form.Control
-        className="w-75"
-        placeholder={`Filter services by ${filterType} name`}
-        value={filterValue}
-        onChange={(event) => changeFilterValue(filterType, event.target.value)}
-      />
-      <Button
-        variant={filterValue ? 'danger' : 'outline-secondary'}
-        onClick={() => changeFilterValue(filterType, '')}
-        disabled={!filterValue}
-      >
-        <FontAwesomeIcon icon="times" />
-      </Button>
-    </InputGroup>
+    <Form className="mb-2" data-bs-theme={variant}>
+      <InputGroup className="d-flex flex-nowrap">
+        <InputGroup.Text>
+          <FontAwesomeIcon icon="filter" />
+        </InputGroup.Text>
+        <Form.Select
+          className="w-auto"
+          value={filterType}
+          onChange={(event) => {
+            changeFilterType(event.target.value)
+          }}
+        >
+          <option value="service">Service</option>
+          <option value="stack">Stack</option>
+        </Form.Select>
+        <Form.Control
+          className="w-75"
+          placeholder={`Filter services by ${filterType} name`}
+          value={filterValue}
+          onChange={(event) =>
+            changeFilterValue(filterType, event.target.value)
+          }
+        />
+        <Button
+          variant={filterValue ? 'danger' : 'outline-secondary'}
+          onClick={() => changeFilterValue(filterType, '')}
+          disabled={!filterValue}
+        >
+          <FontAwesomeIcon icon="times" />
+        </Button>
+      </InputGroup>
+    </Form>
   )
 }
 
