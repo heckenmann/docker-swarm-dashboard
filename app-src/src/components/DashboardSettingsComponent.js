8b8a180124055d911ddccb883e59c0718243f0ce
heckenmann <benedikt.stoffel@heckenmann.de>
2024-09-14 09:27:13 +0200

    add FilterComponent to TimelineComponent

--- app-src/src/components/DashboardSettingsComponent.js
+++ app-src/src/components/DashboardSettingsComponent.js
@@ -1,50 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useAtom } from 'jotai'
-import {
-  Button,
-  ButtonGroup,
-  Col,
-  Form,
-  InputGroup,
-  Row,
-} from 'react-bootstrap'
+import { Button, ButtonGroup, Col, Form, Row } from 'react-bootstrap'
 import { dashboardHId, dashboardVId } from '../common/navigationConstants'
 import {
-  serviceNameFilterAtom,
+  currentVariantAtom,
   dashboardSettingsDefaultLayoutViewIdAtom,
   viewAtom,
-  stackNameFilterAtom,
-  currentVariantAtom,
 } from '../common/store/atoms'
 import { useAtomValue } from 'jotai/index'
-import { useState } from 'react'
+import { FilterComponent } from './FilterComponent'
 
 function DashboardSettingsComponent() {
   const [view, updateViewId] = useAtom(viewAtom)
   const defaultLayout = useAtomValue(dashboardSettingsDefaultLayoutViewIdAtom)
-  const [serviceFilter, setServiceNameFilter] = useAtom(serviceNameFilterAtom)
-  const [stackFilter, setStackNameFilter] = useAtom(stackNameFilterAtom)
   const variant = useAtomValue(currentVariantAtom)
-  const [filterType, setFilterType] = useState('service')
-  const [filterValue, setFilterValue] = useState(
-    `${serviceFilter}${stackFilter}`,
-  )
-
-  const changeFilterType = (filterType) => {
-    setFilterType(filterType)
-    changeFilterValue(filterType, filterValue)
-  }
-
-  const changeFilterValue = (filterType, filterValue) => {
-    setFilterValue(filterValue)
-    if (filterType === 'service') {
-      setStackNameFilter('')
-      setServiceNameFilter(filterValue)
-    } else if (filterType === 'stack') {
-      setServiceNameFilter('')
-      setStackNameFilter(filterValue)
-    }
-  }
 
   const vertical =
     view?.id == dashboardVId || (!view.id && defaultLayout == dashboardVId)
@@ -69,29 +38,7 @@ function DashboardSettingsComponent() {
             </ButtonGroup>
           </Col>
           <Col xs={7}>
-            <InputGroup>
-              <InputGroup.Text>
-                <FontAwesomeIcon icon="filter" />
-              </InputGroup.Text>
-              <Form.Select
-                className="w-auto"
-                value={filterType}
-                onChange={(event) => {
-                  changeFilterType(event.target.value)
-                }}
-              >
-                <option value="service">Service</option>
-                <option value="stack">Stack</option>
-              </Form.Select>
-              <Form.Control
-                className="w-75"
-                placeholder={`Filter services by ${filterType} name`}
-                value={filterValue}
-                onChange={(event) =>
-                  changeFilterValue(filterType, event.target.value)
-                }
-              />
-            </InputGroup>
+            <FilterComponent />
           </Col>
         </Row>
       </Form>
---
+++ app-src/src/components/FilterComponent.js
@@ -0,0 +1,68 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, Form, InputGroup } from 'react-bootstrap'
+import { useState } from 'react'
+import { useAtom } from 'jotai/index'
+import {
+  serviceNameFilterAtom,
+  stackNameFilterAtom,
+} from '../common/store/atoms'
+
+function FilterComponent() {
+  const [serviceFilter, setServiceNameFilter] = useAtom(serviceNameFilterAtom)
+  const [stackFilter, setStackNameFilter] = useAtom(stackNameFilterAtom)
+  const [filterType, setFilterType] = useState(
+    serviceFilter ? 'service' : stackFilter ? 'stack' : 'service',
+  )
+  const [filterValue, setFilterValue] = useState(
+    `${serviceFilter}${stackFilter}`,
+  )
+
+  const changeFilterType = (filterType) => {
+    setFilterType(filterType)
+    changeFilterValue(filterType, filterValue)
+  }
+
+  const changeFilterValue = (filterType, filterValue) => {
+    setFilterValue(filterValue)
+    if (filterType === 'service') {
+      setStackNameFilter('')
+      setServiceNameFilter(filterValue)
+    } else if (filterType === 'stack') {
+      setServiceNameFilter('')
+      setStackNameFilter(filterValue)
+    }
+  }
+
+  return (
+    <InputGroup className="d-flex flex-nowrap">
+      <InputGroup.Text>
+        <FontAwesomeIcon icon="filter" />
+      </InputGroup.Text>
+      <Form.Select
+        className="w-auto"
+        value={filterType}
+        onChange={(event) => {
+          changeFilterType(event.target.value)
+        }}
+      >
+        <option value="service">Service</option>
+        <option value="stack">Stack</option>
+      </Form.Select>
+      <Form.Control
+        className="w-75"
+        placeholder={`Filter services by ${filterType} name`}
+        value={filterValue}
+        onChange={(event) => changeFilterValue(filterType, event.target.value)}
+      />
+      <Button
+        variant={filterValue ? 'danger' : 'outline-secondary'}
+        onClick={() => changeFilterValue(filterType, '')}
+        disabled={!filterValue}
+      >
+        <FontAwesomeIcon icon="times" />
+      </Button>
+    </InputGroup>
+  )
+}
+
+export { FilterComponent }
