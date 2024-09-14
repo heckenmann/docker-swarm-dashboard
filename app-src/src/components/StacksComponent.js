16290843c0b31f54235dfeacd67d42ad45b1b2aa
heckenmann <benedikt.stoffel@heckenmann.de>
2024-09-14 09:58:17 +0200

    add FilterComponent to StacksComponent.js

--- app-src/src/components/StacksComponent.js
+++ app-src/src/components/StacksComponent.js
@@ -5,76 +5,104 @@ import {
   currentVariantAtom,
   currentVariantClassesAtom,
   dashboardSettingsAtom,
+  serviceNameFilterAtom,
+  stackNameFilterAtom,
   stacksAtom,
   viewAtom,
 } from '../common/store/atoms'
 import { useAtom, useAtomValue } from 'jotai'
 import { servicesDetailId } from '../common/navigationConstants'
+import { FilterComponent } from './FilterComponent'
+
 function StacksComponent() {
   const currentVariant = useAtomValue(currentVariantAtom)
   const currentVariantClasses = useAtomValue(currentVariantClassesAtom)
   const dashBoardSettings = useAtomValue(dashboardSettingsAtom)
+  const serviceNameFilter = useAtomValue(serviceNameFilterAtom)
+  const stackNameFilter = useAtomValue(stackNameFilterAtom)
 
   let stacks
   const stacksData = useAtomValue(stacksAtom)
   const [, updateView] = useAtom(viewAtom)
   let createServicesForStack = (stack) => {
-    return stack['Services'].map((service) => (
-      <tr key={service['ID']}>
-        <td
-          className="cursorPointer text-nowrap"
-          onClick={() => {
-            updateView({ id: servicesDetailId, detail: service['ID'] })
-          }}
-        >
-          {service['ShortName'] ? service['ShortName'] : service['ServiceName']}
-        </td>
-        <td>{service['Replication']}</td>
-        <td>
-          {toDefaultDateTimeString(
-            new Date(service['Created']),
-            dashBoardSettings.locale,
-            dashBoardSettings.timeZone,
-          )}
-        </td>
-        <td>
-          {toDefaultDateTimeString(
-            new Date(service['Updated']),
-            dashBoardSettings.locale,
-            dashBoardSettings.timeZone,
-          )}
-        </td>
-      </tr>
-    ))
+    return stack['Services']
+      .filter((service) =>
+        serviceNameFilter
+          ? service['ShortName'].includes(serviceNameFilter)
+          : true,
+      )
+      .map((service) => (
+        <tr key={service['ID']}>
+          <td
+            className="cursorPointer text-nowrap"
+            onClick={() => {
+              updateView({ id: servicesDetailId, detail: service['ID'] })
+            }}
+          >
+            {service['ShortName']
+              ? service['ShortName']
+              : service['ServiceName']}
+          </td>
+          <td>{service['Replication']}</td>
+          <td>
+            {toDefaultDateTimeString(
+              new Date(service['Created']),
+              dashBoardSettings.locale,
+              dashBoardSettings.timeZone,
+            )}
+          </td>
+          <td>
+            {toDefaultDateTimeString(
+              new Date(service['Updated']),
+              dashBoardSettings.locale,
+              dashBoardSettings.timeZone,
+            )}
+          </td>
+        </tr>
+      ))
   }
-  stacks = stacksData.map((stack) => (
-    <Card
-      bg={currentVariant}
-      className={currentVariantClasses + ' mb-3'}
-      key={'card_' + stack['Name']}
-    >
-      <Card.Header>
-        <h5>
-          <FontAwesomeIcon icon="cubes" /> {stack['Name']}
-        </h5>
-      </Card.Header>
-      <Card.Body>
-        <Table variant={currentVariant} size="sm" striped hover>
-          <thead>
-            <tr>
-              <th>Service Name</th>
-              <th className="col-md-1">Replication</th>
-              <th className="col-md-2">Created</th>
-              <th className="col-md-2">Updated</th>
-            </tr>
-          </thead>
-          <tbody>{createServicesForStack(stack)}</tbody>
-        </Table>
-      </Card.Body>
-    </Card>
-  ))
+  stacks = stacksData
+    .filter((stack) =>
+      stackNameFilter ? stack['Name'].includes(stackNameFilter) : true,
+    )
+    .filter((stack) => createServicesForStack(stack).length > 0)
+    .map((stack) => (
+      <Card
+        bg={currentVariant}
+        className={currentVariantClasses + ' mb-3'}
+        key={'card_' + stack['Name']}
+      >
+        <Card.Header>
+          <h5>
+            <FontAwesomeIcon icon="cubes" /> {stack['Name']}
+          </h5>
+        </Card.Header>
+        <Card.Body>
+          <Table variant={currentVariant} size="sm" striped hover>
+            <thead>
+              <tr>
+                <th>Service Name</th>
+                <th className="col-md-1">Replication</th>
+                <th className="col-md-2">Created</th>
+                <th className="col-md-2">Updated</th>
+              </tr>
+            </thead>
+            <tbody>{createServicesForStack(stack)}</tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    ))
 
-  return <>{stacks}</>
+  return (
+    <>
+      <Card className={currentVariantClasses + ' mb-3'} key={'card_filter'}>
+        <Card.Header>
+          <FilterComponent />
+        </Card.Header>
+      </Card>
+      {stacks}
+    </>
+  )
 }
 
 export { StacksComponent }
