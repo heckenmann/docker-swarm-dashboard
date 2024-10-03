8824c4c211cf337b6e51ab8a8705ce41812436be
heckenmann <benedikt.stoffel@heckenmann.de>
2024-10-03 11:14:52 +0200

    add icons

--- app-src/src/components/TasksComponent.js
+++ app-src/src/components/TasksComponent.js
@@ -14,6 +14,7 @@ import {
 } from '../common/store/atoms'
 import ServiceStatusBadge from './ServiceStatusBadge'
 import { FilterComponent } from './FilterComponent'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 /**
  * TasksComponent is a React functional component that displays a list of tasks
@@ -43,6 +44,9 @@ function TasksComponent() {
         key={'tasksTable-' + task['ID']}
         className={task['State'] === 'failed' ? 'table-danger' : null}
       >
+        <td>
+          <FontAwesomeIcon icon="tasks" />
+        </td>
         <td>
           {toDefaultDateTimeString(
             new Date(task['Timestamp']),
@@ -98,6 +102,7 @@ function TasksComponent() {
       >
         <thead>
           <tr>
+            <th style={{ width: '25px' }}></th>
             <th id="timestampCol">Timestamp</th>
             <th id="stateCol">State</th>
             <th id="desiredstateCol">DesiredState</th>
