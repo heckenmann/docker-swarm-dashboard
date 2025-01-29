90ab5d8737c2012b46a482b385768af9abc6815a
heckenmann <heckenmann@users.noreply.github.com>
2025-01-29 12:25:53 +0000

    fix open browser for devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -7,5 +7,5 @@ yarn install
 
 # Cypress
 apt-get update
-apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+apt-get install -y libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb xdg-utils
 yarn cypress install
