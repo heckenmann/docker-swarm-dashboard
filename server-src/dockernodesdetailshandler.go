5e419b00505d60aa31aad4140b1189c6277a6e8f
heckenmann <benedikt.stoffel@heckenmann.de>
2026-01-25 22:12:32 +0100

    backend: update service/node log handlers

--- server-src/dockernodesdetailshandler.go
+++ server-src/dockernodesdetailshandler.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"net/http"
+	"sort"
+
 	"github.com/docker/docker/api/types/filters"
 	"github.com/gorilla/mux"
-	"net/http"
 
 	"github.com/docker/docker/api/types"
 )
@@ -22,7 +24,26 @@ func dockerNodesDetailsHandler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	if len(Services) == 1 {
-		jsonString, _ := json.Marshal(Services[0])
+		// Get tasks for this node
+		tasksFilter := filters.NewArgs()
+		tasksFilter.Add("node", paramNodeId)
+		Tasks, err := cli.TaskList(context.Background(), types.TaskListOptions{Filters: tasksFilter})
+		if err != nil {
+			// If task list fails in the mock environment, return node without tasks
+			Tasks = nil
+		}
+		// Sort tasks by UpdatedAt descending
+		sort.Slice(Tasks, func(i, j int) bool {
+			return Tasks[i].UpdatedAt.After(Tasks[j].UpdatedAt)
+		})
+		// For compatibility with tests that expect the node object at top-level,
+		// merge node fields into the top-level response and include tasks.
+		var response map[string]interface{}
+		// marshal then unmarshal node to generic map
+		nodeBytes, _ := json.Marshal(Services[0])
+		json.Unmarshal(nodeBytes, &response)
+		response["tasks"] = Tasks
+		jsonString, _ := json.Marshal(response)
 		w.Write(jsonString)
 	} else {
 		w.Write([]byte("{}"))
