53333b8700d1104b069620e9b0078f27483500c1
heckenmann <benedikt.stoffel@heckenmann.de>
2026-01-25 22:12:32 +0100

    ui: update store and entity actions

--- app-src/src/common/actions/entityActions.js
+++ app-src/src/common/actions/entityActions.js
@@ -18,20 +18,27 @@ import {
   filterTypeAtom,
   viewAtom,
 } from '../store/atoms'
-import { servicesDetailId, nodesDetailId } from '../navigationConstants'
+import { servicesDetailId, nodesDetailId, tasksId } from '../navigationConstants'
 
 export function useEntityActions(entityType = 'service') {
   const [, updateView] = useAtom(viewAtom)
   const [, setServiceFilterName] = useAtom(serviceNameFilterAtom)
   const [, setStackFilterName] = useAtom(stackNameFilterAtom)
   const [, setFilterType] = useAtom(filterTypeAtom)
+  // We store the selected detail id on the shared `viewAtom` under the
+  // `detail` key instead of using separate detail-id atoms (those aren't
+  // exported). Update the view atom atomically so components/readers that
+  // look at `viewAtom.detail` pick up the id.
 
   const onOpen = (detailId) => {
     if (!detailId) return
     if (entityType === 'node') {
-      updateView({ id: nodesDetailId, detail: detailId })
-    } else {
-      updateView({ id: servicesDetailId, detail: detailId })
+      // set the dedicated node detail id atom and navigate to nodes detail view
+  updateView((prev) => ({ ...(prev || {}), id: nodesDetailId, detail: detailId }))
+    } else if (entityType === 'service') {
+  updateView((prev) => ({ ...(prev || {}), id: servicesDetailId, detail: detailId }))
+    } else if (entityType === 'task') {
+  updateView((prev) => ({ ...(prev || {}), id: tasksId, detail: detailId }))
     }
   }
 
