c6f340a1a1250c1f8c1039ca3646ac398e407ad9
heckenmann <benedikt.stoffel@heckenmann.de>
2026-01-26 08:50:45 +0100

    test: remove non-existing optional test entries from atoms.combined.test.js

--- app-src/tests/unit/common/atoms.combined.test.js
+++ app-src/tests/unit/common/atoms.combined.test.js
@@ -6,18 +6,7 @@ require('./atoms_theme.test.js')
 require('./atoms_logic.test.js')
 require('./atoms_branches.test.js')
 
-// parsing & guards (optional)
-try { require('./atoms_parsing.test.js') } catch (e) {}
-try { require('./atoms_parse_edgecases.test.js') } catch (e) {}
-try { require('./atoms_guards.test.js') } catch (e) {}
-
 // fetch/runtime/module-load
-try { require('./fetch_atoms.combined.test.js') } catch (e) {}
-try { require('./atoms_runtime.test.js') } catch (e) {}
-try { require('./atoms_module_load.combined.test.js') } catch (e) {}
+require('./fetch_atoms.combined.test.js')
 
-// extra branches and variants
-try { require('./atoms_additional_branches.test.js') } catch (e) {}
-try { require('./atoms_hash_variants.test.js') } catch (e) {}
-try { require('./atoms_and_jsontable_extra.test.js') } catch (e) {}
 
