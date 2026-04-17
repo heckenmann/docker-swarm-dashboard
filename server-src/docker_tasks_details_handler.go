b1cc940b2f504b39f1f6bb2865c7f404ebcb1921
heckenmann <benedikt.stoffel@heckenmann.de>
2026-04-17 22:35:51 +0200

    feat(ui): display service and node names with IDs in parentheses in task details

--- server-src/docker_tasks_details_handler.go
+++ server-src/docker_tasks_details_handler.go
@@ -10,19 +10,44 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Serves single node
+// Serves single task
 func dockerTasksDetailsHandler(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	paramTaskId := params["id"]
 	cli := getCli()
 	tasksFilter := filters.NewArgs()
 	tasksFilter.Add("id", paramTaskId)
-	Services, err := cli.TaskList(context.Background(), swarm.TaskListOptions{Filters: tasksFilter})
+	Tasks, err := cli.TaskList(context.Background(), swarm.TaskListOptions{Filters: tasksFilter})
 	if err != nil {
 		panic(err)
 	}
-	if len(Services) == 1 {
-		jsonString, _ := json.Marshal(Services[0])
+	if len(Tasks) == 1 {
+		t := Tasks[0]
+		// convert task to a generic map first
+		var tm map[string]interface{}
+		b, _ := json.Marshal(t)
+		_ = json.Unmarshal(b, &tm)
+
+		// try to fetch node object for this task
+		nodesFilter := filters.NewArgs()
+		nodesFilter.Add("id", t.NodeID)
+		nodeList, _ := cli.NodeList(context.Background(), swarm.NodeListOptions{Filters: nodesFilter})
+		if len(nodeList) > 0 {
+			tm["NodeName"] = nodeList[0].Description.Hostname
+			if tm["NodeName"] == "" {
+				tm["NodeName"] = nodeList[0].ID
+			}
+		}
+
+		// try to fetch service object for this task
+		servicesFilter := filters.NewArgs()
+		servicesFilter.Add("id", t.ServiceID)
+		serviceList, _ := cli.ServiceList(context.Background(), swarm.ServiceListOptions{Filters: servicesFilter})
+		if len(serviceList) > 0 {
+			tm["ServiceName"] = serviceList[0].Spec.Name
+		}
+
+		jsonString, _ := json.Marshal(tm)
 		_, _ = w.Write(jsonString)
 	} else {
 		_, _ = w.Write([]byte("{}"))
